7937ad82d34d8182496b7ea267c1cf0ee1fd6fb7
sonht <sonht1109@gmail.com>
2022-04-06 22:34:11 +0700

    enable cors

--- src/main/java/com/dacanh/SQANhapHang/dao/DAO.java
+++ src/main/java/com/dacanh/SQANhapHang/dao/DAO.java
@@ -8,11 +8,12 @@ public class DAO {
 	public DAO(){
 	       if(con == null){
 	           String dbUrl = "jdbc:mysql://localhost:3306/sqa_nhaphang?autoReconnect=true&useSSL=false";
-	           String dbClass = "com.mysql.jdbc.Driver";
+	           String dbClass = "com.mysql.cj.jdbc.Driver";
 	 
 	           try {
 	               Class.forName(dbClass);
-	               con = DriverManager.getConnection (dbUrl, "root", "CNPM@2021");
+	               con = DriverManager.getConnection (dbUrl, "sonht",
+						   "htson2000");
 	           }catch(Exception e) {
 	               e.printStackTrace();
 	           }
--- src/main/java/com/dacanh/SQANhapHang/repository/ImportOrderRepository.java
+++ src/main/java/com/dacanh/SQANhapHang/repository/ImportOrderRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import com.dacanh.SQANhapHang.model.ImportOrder;
 
 public interface ImportOrderRepository extends JpaRepository<ImportOrder, Integer> {
-	@Query(value = "SELECT iod.id, iod.code, iod.date, iod.staff_id, iod.supplier_id FROM ImportOrder AS iod", nativeQuery = true)
+	@Query(value = "SELECT iod.id, iod.code, iod.date, iod.staff_id, iod" +
+			".supplier_id FROM importorder AS iod", nativeQuery = true)
 	public List<ImportOrder> findAll();
 }
